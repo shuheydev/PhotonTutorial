1874607256c563ab66b703a724272a9e4db1068b
shuhei.nishizawa <shuheydev@gmail.com>
2021-11-25 16:34:41 +0900

    チュートリアル3:Roomを4つと、退室ボタンの用意

---
+++ Assets/MyResources/OfficialPhotonTutorial/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+namespace Com.Harusoft.PhotonTutorial
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Photone Callbacks
+
+        /// <summary>
+        /// ローカルのプレイヤーがRoomを退出するときに呼ばれる。
+        /// LauncherScene(ユーザー名を入力する画面)を表示する
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            //base.OnLeftRoom();
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
